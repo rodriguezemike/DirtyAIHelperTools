0fae9cc7d925ebe6c6561bc2ecc1351a82a9a5d9
Michael Rodriguez <rodriguez.e.mike@gmail.com>
2025-04-08 17:19:08 -0700

    Added functionality to save out to requirements.txt

--- python_to_pip.py
+++ python_to_pip.py
@@ -8,17 +8,26 @@ import os
 #This should be used to generate a pip install command from looking through python files in a given directory.
 
 dotenv.load_dotenv()
+run_pip_requirements_generation = False
 run_pip_install = False
 run_pip_requirements_freeze = False
 
-import_grep_command = 'grep -rio "^import \w*" %s' % sys.argv[1]
-from_grep_command = 'grep -rio "^from \w*" %s' % sys.argv[1]
+import_grep_command = 'grep -rio "^import \w*" %s' % os.path.abspath(sys.argv[1])
+from_grep_command = 'grep -rio "^from \w*" %s' % os.path.abspath(sys.argv[1])
+
+print("Import Command %s" % import_grep_command)
+print("From Grep Command %s" % from_grep_command)
 
 import_process = subprocess.run(shlex.split(import_grep_command), capture_output = True)
 from_process = subprocess.run(shlex.split(from_grep_command), capture_output = True)
 
+imports = [i.decode("utf-8").split(":")[1].strip() for i in import_process.stdout.splitlines() if ":" in i.decode("utf-8")]
 
-import_list = list(set([i.decode("utf-8").split(":")[1].strip() for i in import_process.stdout.splitlines()] + [i.decode("utf-8").split(":")[1].strip().replace("from", "import") for i in from_process.stdout.splitlines()]))
+from_imports = [i.decode("utf-8").split(":")[1].strip().replace("from", "import") for i in from_process.stdout.splitlines() if ":" in i.decode("utf-8")]
+
+import_list = list(set(imports + from_imports))
+
+print("Import List -> %s"% import_list)
 
 prompt = "Generate a pip install command of the available PyPI packages from the following import list excluding internal packages or non-PyPI packages with no explanation %s" % ",".join(import_list)
 
@@ -26,7 +35,7 @@ print("Prompt -> %s\n\n" % prompt)
 
 
 client = OpenAI(
-  api_key=os.environ["OPENAI_API_KEY"]
+  api_key=os.environ["POPENAI_API_KEY"]
 )
 
 completion = client.chat.completions.create(
@@ -47,6 +56,14 @@ for content in completion.choices[0].message.content.split("\n"):
 print("pip command\n")
 print(pip_command)
 print()
+
+if run_pip_requirements_generation:
+	python_directory = os.path.dirname(os.path.abspath(sys.argv[1]))
+	requirements_path = os.path.join(python_directory, "requirements.txt")
+	packages = pip_command.split("install")[1].split()
+	with open(requirements_path, 'w') as f:
+		f.write("\n".join(packages))
+	print("Wrote requirements file at %s" % requirements_path)
 
 if run_pip_install:
 	print("Running auto install. I hope you know what youre doing")
